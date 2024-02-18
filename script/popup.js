5edd4a17668555d64876f67fdfe0bc9d35cc5f8a
Pedro H. Mota <110642021+mota-pedro@users.noreply.github.com>
2024-02-18 12:12:00 -0300

    feat: ler conteúdo da tab ativa

--- script/popup.js
+++ script/popup.js
@@ -1,8 +1,25 @@
 const textInput = document.getElementById('text-input');
+const copyButton = document.getElementById('copy-button');
 const readButton = document.getElementById('read-button');
 
-readButton.addEventListener('click', readText);
+copyButton.addEventListener('click', () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        const tab = tabs[0];
 
-function readText() {
-    chrome.tts.speak(textInput.value);
-}
+        function copyText() {
+            const pageText = document.body.innerText;
+            chrome.storage.local.set({ 'pageText': pageText});
+            console.log(pageText);
+        };
+
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            func: copyText
+        });
+    });
+
+    chrome.storage.local.get('pageText', (data) => {
+        const content = data.pageText;
+        chrome.tts.speak(content);
+    })
+});
